c3d5a2fc543ab4a52fe66cc13cf056358aa9623e
Jerson Janke <jerson.janke@saudeid.com.br>
2022-10-04 16:56:28 -0300

    test npm

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = { 
+    all: [ "JavaScript",
+    "Java",
+    "Python",
+    "C#",
+    "PHP",
+    "C++",
+    "C",
+    "Typescript",
+    "Ruby",
+    "Swift",
+    "Assembly",
+    "Go",
+    "Cobol",
+    "R",
+    "Pearl"
+    ];
+}
